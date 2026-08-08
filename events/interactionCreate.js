a69916aeb05b5ad38724cdf2c243be6501fdf8cf
Neel Bapat <neelb789@gmail.com>
2026-08-09 03:40:21 +0530

    added reload command for quicker testing in server

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -41,16 +41,20 @@ module.exports = {
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({
-                    content: 'There was an error while executing this command!',
-                    flags: MessageFlags.Ephemeral,
-                });
-            } else {
-                await interaction.reply({
-                    content: 'There was an error while executing this command!',
-                    flags: MessageFlags.Ephemeral,
-                });
+            try {
+                if (interaction.replied || interaction.deferred) {
+                    await interaction.followUp({
+                        content: 'There was an error while executing this command!',
+                        flags: MessageFlags.Ephemeral,
+                    });
+                } else {
+                    await interaction.reply({
+                        content: 'There was an error while executing this command!',
+                        flags: MessageFlags.Ephemeral,
+                    });
+                }
+            } catch (e) {
+                console.error('Failed to send error response:', e);
             }
         }
     },
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// Require the necessary discord.js classes
+// require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
+// create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-
+client.cooldowns = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -18,7 +18,7 @@ for (const folder of commandFolders) {
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        // set a new item in the collection with the key as the command name and the value as the exported module
         if ('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
         } else {
@@ -40,5 +40,5 @@ for (const file of eventFiles) {
     }
 }
 
-// Log in to Discord with your client's token
+// log in to discord with the client's token
 client.login(token);
